cd11d6be90e82df3b2a594663b70d447add9e571
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-11-13 21:37:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const parseCode = (str) => {
   // your code here
+	str= str.split("0").filter(s => s.length!==0);
+	return { firstName: str[0], lastName: str[1], id: str[2] }
 };
 
 // Do not change the code below
 const str = prompt("Enter str: ");
 alert(JSON.stringify(parseCode(str)));
+//parseCode("John000Doe000123");
+
+
+
+
